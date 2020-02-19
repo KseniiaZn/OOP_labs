fbfdea922e30675c42d2fe6fc566cb39fd2b9e7c
Kseniia <ksenia.zinkova@gmail.com>
2020-02-19 10:41:02 +0200

    first lab

---
+++ OOP_lab_1_C++.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+void title();         //відомості про автора
+void part_1(int& a);  //Лiчильник збiльшення на 1.
+int part_2();         //Завдання 2. Oперацiя вiдношення "=="
+
+int main()
+{
+	setlocale(LC_CTYPE, "ukr");
+	title();
+	cout << "Завдання 1. Лiчильник збiльшення на 1." << endl << "Введiть операнд: ";
+	int x;
+	cin >> x;
+	part_1(x);
+	cout <<endl << "Завдання 2. Oперацiя вiдношення \"==\"" << endl;
+	part_2();
+}
+void part_1(int& a) {
+	int idx = 0;
+	while ((a & (1 << idx)) > 0) //перебираємо числа(ind), доки кон'юнкція А з pow(2, ind) дає додатнє число
+		idx++;
+	a ^= ((1 << (idx + 1)) - 1); //застосовуємо виключаюче або до А та pow(2, ind + 1) - 1
+	cout << "Результат: " << a << endl;
+}
+int part_2() {
+	int a, b;
+	bool rez;
+	cout << "Введiть перший операнд: ";
+	cin >> a;
+	cout << "Введiть другий операнд: ";
+	cin >> b;
+	if (!(a ^ b)) {//порівняння операндів виключаючим "або"
+		cout << "Операцiя вiдношення \"==\" виконується" << endl;
+		rez = true;
+	}
+	else {
+		cout << "Операцiя вiдношення \"==\" не виконується" << endl;
+		rez = false;
+	}
+	return rez;
+}	
+void title() {
+	cout << "Лабоаторна робота №1 з дисциплiни ООП" << endl << "  студентки IС-92, Зiнкової Ксенiї" << endl;
+	for (int i = 0; i < 37; i++) {
+		cout << "=";
+	} cout << endl;
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace OOP_lab_1_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            title();        //відомості про автора
+            int a;
+            Console.WriteLine("Завдання 1. Лiчильник збiльшення на 1\nВведiть операнд: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Part_1(ref a);  //Лiчильник збiльшення на 1.
+            Console.WriteLine("Завдання 2. Oперацiя вiдношення \"==\"");
+            Part_2();       //Завдання 2. Oперацiя вiдношення "=="
+        }
+        static void title()
+        {
+            Console.WriteLine("Лабоаторна робота №1 з дисциплiни ООП \n  студентки IС-92, Зiнкової Ксенiї \n           " +
+                " варiант 5\n======================================");
+        }
+        static void Part_1(ref int a)
+        {
+            int idx = 0;
+            while ((a & (1 << idx)) > 0)
+                idx++;
+            a ^= ((1 << (idx + 1)) - 1);
+            Console.WriteLine("Результат: " + a);
+        }
+        static int Part_2()
+        {
+            int b, c;
+            int rez;
+            Console.WriteLine("Введiть перший операнд: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введiть другий операнд: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            if ((b ^ c) == 0)   //порівняння операндів виключаючим "або"
+            {
+                Console.WriteLine("Операцiя вiдношення \"==\" виконується");
+                rez = 1;
+            }
+            else
+            {
+                Console.WriteLine("Операцiя вiдношення \"==\" не виконується");
+                rez = 0;
+            }
+            return rez;
+        }
+    }
+}
