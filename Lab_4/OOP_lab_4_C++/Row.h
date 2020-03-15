2208ba288a1090570ca2ae8c374164d52e5cc8db
Kseniia <32236255+KseniiaZn@users.noreply.github.com>
2020-03-15 18:33:36 +0200

    Lab 4

---
+++ Lab_4/OOP_lab_4_C++/Row.h
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class Row {
+private:
+	int* _row = {};
+	int _size;
+public:
+
+	int length();
+	Row(); //конструктор за замовчуванням - 10 рандомних чисел
+	Row(char* row, int n); //конструктор, в який передаємо готовий масив
+	Row(const Row& other); //конструктор копіювання
+	~Row();
+
+	void init(char* arr, int n); //ініціалізація об'єкта переданим масивом
+	void print();
+
+	void Rewrite(const Row& other); //конструктор копіювання
+
+	Row operator +(const Row& other); //превантеження оператора "+" щоб складати 2 об'єкти
+	Row operator -(int n); //превантеження оператора "-" віднімати якесь число від елементів об'єкта
+};
---
+++ Lab_4/OOP_lab_4_Csharp/Program.cs
@@ -0,0 +1,35 @@
+﻿// OOP_lab_4_C# || Zinkova Kseniia || IS-92
+using System;
+
+namespace OOP_lab_4_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 =  { 1, 2, 3, 5, 6, 7, 8, 9, 10 };
+
+            Row C1 = new Row(arr1, 9);
+            Row C2 = new Row();
+            Row C3 = new Row(C2);
+
+            Console.WriteLine("Given rows: ");
+            C1.print();
+            C2.print();
+            C3.print();
+
+            C2.Rewrite(C2 - 5);
+            Console.WriteLine("\n Rows after deducting 5 from C2: ");
+            C1.print();
+            C2.print();
+            C3.print();
+
+            C1.Rewrite(C2 + C3);
+            Console.WriteLine("\n Rows after substitutions C1 with the sum of C2 & C3: ");
+            C1.print();
+            C2.print();
+            C3.print();
+        }
+        
+    }
+}
