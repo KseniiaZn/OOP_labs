779857d10ae7e15a22c3aa67926c660efc7fbaa1
Kseniia <32236255+KseniiaZn@users.noreply.github.com>
2020-05-07 22:13:54 +0300

    lab 8, C++

---
+++ OOP_lab_8_C++/OOP_lab_8_C++.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int numb_quant(string row); //ф-ція визначення кількості цифр в рядку
+
+int main() {
+	setlocale(LC_CTYPE, "ukr");
+	cout << "Зiнкова Ксенiя, IC-92" << endl;
+	string row;
+	cout << "Задайте рядок: ";
+	getline(cin, row);   //отримуємо рядок від користувача
+
+	int(*fooPointer1)(string row);  //оголошую вказівник на ф-цію з заданими параметрами
+	fooPointer1 = numb_quant;       //присвоюю вказівнику ф-цію
+
+	cout << "Кiлькiсть цифер у введеному рядку: "<<fooPointer1(row); //викликаю ф-цію через вказівник на неї
+
+	return 0;
+}
+int numb_quant(string row) {
+	int counter = 0;
+	for (int i = 0; i < row.length(); i++) {
+		if (row[i] >= 48 && row[i] <= 57) {    //рахуємо к-ть цифер, повертаємо її функцією
+			counter++;
+		}
+	}
+	return counter;
+}
